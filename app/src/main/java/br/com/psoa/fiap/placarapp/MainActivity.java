adf175ee4db424b896a96971d13bc676ee713695
logonrm <logonrm@fiap.com.br>
2017-12-04 23:06:11 -0200

    Initial commit

---
+++ app/src/main/java/br/com/psoa/fiap/placarapp/MainActivity.java
@@ -0,0 +1,38 @@
+package br.com.psoa.fiap.placarapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etTimeCasa;
+    private EditText etTimeVisitante;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etTimeCasa = (EditText) findViewById(R.id.eTime_Casa);
+        etTimeVisitante = (EditText) findViewById(R.id.eTime_Visitante);
+    }
+
+    public void comecarJogo(View v) {
+        if(etTimeCasa.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Informe o time da casa", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(etTimeVisitante.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Informe o time visitante", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent proximaTela = new Intent(this, GameActivity.class);
+        proximaTela.putExtra("TIMEVISITANTE", etTimeVisitante.getText().toString());
+        proximaTela.putExtra("TIMECASA", etTimeCasa.getText().toString());
+        startActivity(proximaTela);
+    }
+}
